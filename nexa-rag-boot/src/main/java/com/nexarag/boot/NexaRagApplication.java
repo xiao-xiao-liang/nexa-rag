5c3f5abf49d9931f37a9009b44d2d6390ea2ba79
xiao-xiao-liang <2901815578@qq.com>
2026-08-07 21:06:23 +0800

    fix(boot): 更新启动扫描与告警配置

--- nexa-rag-boot/src/main/java/com/nexarag/boot/NexaRagApplication.java
+++ nexa-rag-boot/src/main/java/com/nexarag/boot/NexaRagApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @MapperScan({
         "com.nexarag.document.mapper",
-        "com.nexarag.document.mapper",
         "com.nexarag.model.mapper",
         "com.nexarag.chat.mapper"
 })
